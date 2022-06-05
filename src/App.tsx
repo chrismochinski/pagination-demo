33e68820a1502ec65bc097a0c1152a8c41d314ef
Chris Mochinski <cmochinski@gmail.com>
2022-06-04 20:48:38 -0500

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const logo = "https://freesvg.org/img/Gerald_G_Fast_Food_Lunch_Dinner_(FF_Menu)_6.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>Wut it do.</p>
+        <a className="App-link" href="http://chrismochinski.com" target="_blank" rel="noopener noreferrer">
+          Mo
         </a>
       </header>
     </div>
